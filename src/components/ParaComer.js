120771c4b3acff8e100ce1f8ce9eb3e15c22a012
Patt0lino <investmentmoises@gmail.com>
2022-06-26 16:08:59 -0300

    módulo card

--- src/components/ParaComer.js
+++ src/components/ParaComer.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from './ParaComer.module.css'
+import BatataFrita from '../images/batata_frita.jpg'
 
 const ParaComer = () => {
   return (
-    <div>
-        <h1>sasasa</h1>
+    <div className={styles.card}>
+        <p>Batata Frita</p>
+        <img src={BatataFrita} alt="" />
+        <p>R$: 10,99</p>
+        <details open='open'>
+        <summary>Composição</summary>
+            <p>- Corte artesanal</p>
+            <p>- Cheddar</p>
+            <p>- Bacon</p>
+            <p>- Queijo prato</p>
+            <p>- Maionese</p>
+        </details>
     </div>
   )
 }
